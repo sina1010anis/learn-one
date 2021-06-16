d39cef7abf35afa9ab9f8aba31af794df227bd30
sina1010anis <sina1010anis@gmail.com>
2021-06-16 13:10:16 -0700

    create view item

--- resources/views/front/include/item.blade.php
+++ resources/views/front/include/item.blade.php
@@ -1,19 +1,18 @@
 <div class="view-items w-100">
-    <ul class="ul-not">
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-    </ul>
+    <span class="one-item">
+        <img src="{{url('img/img/banner.jpg')}}" alt="name" title="laravel">
+        <p class="set-font f-12 text-right" align="right" dir="rtl">
+            <span class="fl-right color-b-900">اموزش laravel</span>
+            <span class="fl-left color-b-600">4.2</span>
+        </p>
+        <br>
+        <p class="set-font f-12 text-right" align="right" dir="rtl">
+            <span class="fl-right color-b-900">قیمت </span>
+            <span class="fl-left color-b-600">25.000</span>
+        </p>
+        <br>
+        <br>
+        <a class="btn-dec set-font f-12 btn-setting-item" href="">برسی محصول</a>
+        <a class="btn-buy set-font f-12 btn-setting-item" href="">خرید محصول</a>
+    </span>
 </div>
