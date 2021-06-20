9cfb817aeb2bc971aed57c3ecc5b201bcc9ce5e8
sina1010anis <sina1010anis@gmail.com>
2021-06-20 07:35:14 -0700

    Fix

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ use App\Http\Controllers\BuyController;
 Route::get('/', [IndexController::class , 'index'])->name('IndexPage');
 Route::get('/item/{name}', [IndexController::class , 'ViewItem'])->name('ViewItem');
 Route::post('/show/item', [IndexController::class , 'ShowItem'])->name('ShowItem');
+Route::prefix('')->middleware('auth')->group(function (){
+    Route::get('/buy/item/{type}/{id}', [BuyController::class , 'BuyItem'])->name('BuyItem');
+    Route::get('/verify/back/item/', [BuyController::class , 'VerifyItem'])->name('VerifyItem');
+    Route::get('/verify/back/item/video', [BuyController::class , 'VerifyItemVideo'])->name('VerifyItemVideo');
+    Route::get('/DL/File/{id}', [IndexController::class , 'DLFileVideo'])->name('DLFile');
+    Route::get('/user/article', [IndexController::class , 'articleView'])->name('articleView');
+    Route::get('/Dl/File/article/{id}', [IndexController::class , 'DLFileArticle'])->name('DLFileArticle');
+});
 
-Route::get('/buy/item/{type}/{id}', [BuyController::class , 'BuyItem'])->name('BuyItem')->middleware('auth');
-Route::get('/verify/back/item/', [BuyController::class , 'VerifyItem'])->name('VerifyItem')->middleware('auth');
-Route::get('/verify/back/item/video', [BuyController::class , 'VerifyItemVideo'])->name('VerifyItemVideo')->middleware('auth');
-
-Route::get('/DL/File/{id}', [IndexController::class , 'DLFileVideo'])->name('DLFile')->middleware('auth');
-Route::post('/test', [IndexController::class , 'test'])->name('test');
-Route::get('/user/article', [IndexController::class , 'articleView'])->name('articleView')->middleware('auth');
-Route::get('/Dl/File/article/{id}', [IndexController::class , 'DLFileArticle'])->name('DLFileArticle')->middleware('auth');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
